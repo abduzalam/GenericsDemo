6754541cd59dac12aa6de92417efbf57ba577051
Abdul Kuzhiyil <Abdul.Kuzhiyil@ii-vi.com>
2023-03-06 14:27:11 +0000

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Press enter to shut down..");
+        Console.ReadLine();
+    }
+
+    private static void DemonstrateTextFileStorage()
+    {
+
+    }
+
+    private static void PopulateLists()
+    {
+
+    }
+}
