27d16b0a9bfa01f61b553dce15033b43bd077d89
Abdul Kuzhiyil <Abdul.Kuzhiyil@ii-vi.com>
2023-03-06 17:10:11 +0000

    Initial commit
    Adding basic building blocks

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,10 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleUI.Models;
 using System.Runtime.CompilerServices;
 
 public class Program
 {
     private static void Main(string[] args)
     {
+        //Console.WriteLine($"DateTime.Now = {DateTime.Now} , DateTime.UtcNow = {DateTime.UtcNow}");//both print same result
+        
         Console.WriteLine();
         Console.WriteLine("Press enter to shut down..");
         Console.ReadLine();
@@ -15,8 +18,15 @@ public class Program
 
     }
 
-    private static void PopulateLists()
+    private static void PopulateLists(List<Person> people, List<LogEntry> logs)
     {
+        people.Add(new Person() { FirstName = "AbdulSalam", LastName = "Abd" });
+        people.Add(new Person() { FirstName = "Steve", LastName = "Jobs", IsAlive = false });
+        people.Add(new Person() { FirstName = "Bill", LastName = "Gates" });
 
+        logs.Add(new LogEntry() { Message="I blew up", ErrorCode= 9999 });
+        logs.Add(new LogEntry() { Message = "I'm too awesome", ErrorCode = 1337 });
+        logs.Add(new LogEntry() { Message = "I was tired", ErrorCode = 2222 });
     }
+  
 }
---
+++ ConsoleUI/WithoutGenerics/OriginalTextFileProcessor.cs
@@ -0,0 +1,75 @@
+﻿using ConsoleUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI.WithoutGenerics
+{
+    public class OriginalTextFileProcessor
+    {
+        private static void DemonstrateTextFileStorage()
+        {
+            List<Person> people = new List<Person>();
+            List<LogEntry> logs = new List<LogEntry>();
+
+            string peopleFile = @"C:\Temp\people.csv";
+            string logFile = @"C:\Temp\logs.csv";
+
+            PopulateLists(people, logs);
+
+            OriginalTextFileProcessor.SavePeople(people,peopleFile);
+
+            var newPeople = OriginalTextFileProcessor.LoadPeople(peopleFile);
+
+            foreach (var p in newPeople)
+            {
+                Console.WriteLine($"{p.FirstName} {p.LastName} (IsAlive = {p.IsAlive})");
+            }
+        }
+
+        private static void PopulateLists(List<Person> people, List<LogEntry> logs)
+        {
+
+        }
+
+        public static List<Person> LoadPeople(string filePath)
+        {
+            List<Person> output = new List<Person>();
+            Person p;
+            var lines = File.ReadAllLines(filePath).ToList();
+
+            //Remove the header row
+
+            lines.RemoveAt(0);
+
+            foreach(var line in lines)
+            {
+                var vals = line.Split(',');
+                p = new Person();
+
+                p.FirstName = vals[0];
+                p.IsAlive = bool.Parse(vals[1]);
+                p.LastName = vals[2];
+
+                output.Add(p);
+            }
+            return output;
+        }
+
+        public static void SavePeople(List<Person> people,string filePath)
+        {
+            List<string> lines = new List<string>();
+
+            //Add header row
+            lines.Add("FirstName,IsAlive,LastName");
+
+            foreach(var p in people)
+            {
+                lines.Add($"{p.FirstName},{p.IsAlive},{p.LastName}");
+            }
+            File.WriteAllLines(filePath,lines);
+        }
+    }
+}
